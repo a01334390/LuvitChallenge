dff85b332a7596f57fe66e42a708301b657325ba
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2020-06-02 23:13:34 -0500

    Added Initial Controller for Data Source
    Added a simple controller to support pagination

--- Luvit-challenge/Luvit-challenge/Controller/RedditFeedController.swift
+++ Luvit-challenge/Luvit-challenge/Controller/RedditFeedController.swift
@@ -8,38 +8,88 @@
 
 import UIKit
 
-enum LoadStatus {
-    case ready (nextSlice: String)
-    case loading (slice: String)
-    case parseError
-    case done
+protocol LoggableClass {
+    func logger(message: String)
 }
 
-class RedditFeed {
-    var redditPosts: [RedditPostData] = [RedditPostData]()
+class RedditFeed : LoggableClass {
+    var redditPosts: [Child] = [Child]()
     var postLimit: Int = 10
-    var after: String = ""
+    var afterHash: String = ""
     var baseURL: String = "https://www.reddit.com/r/all/top/.json?t=all&limit="
-    var loadStatus = LoadStatus.ready(nextSlice: "")
+
     
     init() {
         loadMorePosts()
+    }
+    
+    func logger(message: String) {
+        print("[Reddit Controller] - \(message)")
     }
     
     func buildURL(withSlice slice: String) -> String {
         let sPostLimit: String = String(postLimit)
         let sURL: String = "\(baseURL)\(sPostLimit)"
         
-        return slice.isEmpty ? sURL : "\(sURL)&after=\(after)"
+        return slice.isEmpty ? sURL : "\(sURL)&after=\(afterHash)"
     }
     
-    func loadMorePosts(currentSlice: String? = nil) {
-        guard case let .ready(slice) = loadStatus else {
+    func loadMorePosts() {
+        
+        let completeURL = self.buildURL(withSlice: afterHash)
+        guard let url = URL(string: completeURL), UIApplication.shared.canOpenURL(url) else {
             return
         }
         
-        loadStatus = .loading(slice: "")
-        let completeURL = self.buildURL(withSlice: slice)
+        let task = URLSession.shared.dataTask(with: url,completionHandler: parsePostsFromResponse(data:response:error:))
+        task.resume()
     }
     
+    func parsePostsFromResponse(data: Data?, response: URLResponse?, error: Error?) {
+        guard error == nil else {
+            self.logger(message: "An error occurred while loading: \(error?.localizedDescription ?? "Unknown")")
+            return
+        }
+        
+        guard let data = data else {
+            self.logger(message: "Data might be corrupted")
+            return
+        }
+        
+        let children: [Child] = parsePosts(from: data)
+        
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            self.redditPosts.append(contentsOf: children)
+        }
+    }
+    
+    func parsePosts(from data: Data) -> [Child] {
+        var response : RedditPost
+        do {
+            response = try JSONDecoder().decode(RedditPost.self, from: data)
+        } catch {
+            self.logger(message: error.localizedDescription)
+            return []
+        }
+        return stripPostData(from: response)
+    }
+    
+    func stripPostData(from redditPost: RedditPost) -> [Child] {
+        guard let rData = redditPost.data else {
+            logger(message: "Response is empty.")
+            return []
+        }
+        
+        if let after = rData.after {
+            self.afterHash = after
+        }
+        
+        guard let children = rData.children else {
+            logger(message: "Response returned no children.")
+            return []
+        }
+        
+        return children
+    }
 }
