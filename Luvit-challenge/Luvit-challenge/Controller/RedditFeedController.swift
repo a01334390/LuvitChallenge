03f9c79af7e44b889786585591954ced98350b3a
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2020-06-02 23:21:23 -0500

    Added Controller internal documentation
    Added internal documentation to generate a Doc from Jazzy

--- Luvit-challenge/Luvit-challenge/Controller/RedditFeedController.swift
+++ Luvit-challenge/Luvit-challenge/Controller/RedditFeedController.swift
@@ -13,29 +13,27 @@ protocol LoggableClass {
 }
 
 class RedditFeed : LoggableClass {
+    
+    //MARK: - Properties
+    
     var redditPosts: [Child] = [Child]()
-    var postLimit: Int = 10
-    var afterHash: String = ""
-    var baseURL: String = "https://www.reddit.com/r/all/top/.json?t=all&limit="
+    private var postLimit: Int = 10
+    private var afterHash: String = ""
+    private var baseURL: String = "https://www.reddit.com/r/all/top/.json?t=all&limit="
 
+    // MARK: Initializers
     
     init() {
         loadMorePosts()
     }
     
-    func logger(message: String) {
-        print("[Reddit Controller] - \(message)")
-    }
+    // MARK: - Loading Functions
     
-    func buildURL(withSlice slice: String) -> String {
-        let sPostLimit: String = String(postLimit)
-        let sURL: String = "\(baseURL)\(sPostLimit)"
-        
-        return slice.isEmpty ? sURL : "\(sURL)&after=\(afterHash)"
-    }
-    
+    /**
+     Loads more posts from the Reddit API
+     - Note: All logic regarding pagination is handled by the class. No parameters are required.
+     */
     func loadMorePosts() {
-        
         let completeURL = self.buildURL(withSlice: afterHash)
         guard let url = URL(string: completeURL), UIApplication.shared.canOpenURL(url) else {
             return
@@ -45,7 +43,14 @@ class RedditFeed : LoggableClass {
         task.resume()
     }
     
-    func parsePostsFromResponse(data: Data?, response: URLResponse?, error: Error?) {
+    /**
+     Parses the post response from the URL Session
+     - Parameters:
+        - data: Downloaded data from the URL Session (if any)
+        - response: The URLResponse from the URL Session (if any)
+        - error: Error object from the URLResponse (if any)
+     */
+    private func parsePostsFromResponse(data: Data?, response: URLResponse?, error: Error?) {
         guard error == nil else {
             self.logger(message: "An error occurred while loading: \(error?.localizedDescription ?? "Unknown")")
             return
@@ -64,7 +69,12 @@ class RedditFeed : LoggableClass {
         }
     }
     
-    func parsePosts(from data: Data) -> [Child] {
+    /**
+     Parses data from the URLSession into usable [Child] Objects
+     - Parameter from: Data from the URL Session to be transformed into [Child] Objects
+     - Returns: An array of child objects
+     */
+    private func parsePosts(from data: Data) -> [Child] {
         var response : RedditPost
         do {
             response = try JSONDecoder().decode(RedditPost.self, from: data)
@@ -75,7 +85,12 @@ class RedditFeed : LoggableClass {
         return stripPostData(from: response)
     }
     
-    func stripPostData(from redditPost: RedditPost) -> [Child] {
+    /**
+     Uses the parsed response to retrieve usable information regarding the posts and future API Calls.
+    - Parameter redditPosts: [RedditPost] response parsed from the API
+    - Returns: An array of child objects
+    */
+    private func stripPostData(from redditPost: RedditPost) -> [Child] {
         guard let rData = redditPost.data else {
             logger(message: "Response is empty.")
             return []
@@ -91,5 +106,27 @@ class RedditFeed : LoggableClass {
         }
         
         return children
+    }
+    
+    // MARK: - Helpers
+    
+    /**
+     Simple logging function
+     - Parameter message: Message to be printed by this class
+     */
+    func logger(message: String) {
+        print("[Reddit Controller] - \(message)")
+    }
+    
+    /**
+     Builds an URL from the base URL and passes the required parameters for the petition
+     - Parameter slice: The slice of data to be loaded (retrieved from the API response)
+     - Returns: A string URL
+     */
+    private func buildURL(withSlice slice: String) -> String {
+        let sPostLimit: String = String(postLimit)
+        let sURL: String = "\(baseURL)\(sPostLimit)"
+        
+        return slice.isEmpty ? sURL : "\(sURL)&after=\(afterHash)"
     }
 }
