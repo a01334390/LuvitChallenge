edb48bafec3ecc8c1780833f5cfaeb18a2bbd8dc
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2020-06-02 21:53:26 -0500

    Initial Project Commit
    Added initial project commit

---
+++ Luvit-challenge/Luvit-challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Luvit-challenge
+//
+//  Created by Fernando Martin Garcia Del Angel on 02/06/20.
+//  Copyright © 2020 Fernando Martin Garcia Del Angel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
