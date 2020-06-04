de7653f112839a8467709deee2abbf630d292839
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2020-06-03 23:11:03 -0500

    Added image saving code
    Image saving code complete, now we need to add some feedback.

--- Luvit-challenge/Luvit-challenge/View/ViewController.swift
+++ Luvit-challenge/Luvit-challenge/View/ViewController.swift
@@ -100,8 +100,13 @@ class ViewController: UIViewController, UICollectionViewDelegate {
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        self.redditFeed.redditPosts.remove(at: indexPath.row)
-        self.collectionView.deleteItems(at: [indexPath])
+        if let thumbnail = redditFeed.redditPosts[indexPath.row].data?.thumbnail {
+            NetworkController.shared().downloadImage(from: thumbnail) { thumbnail in
+                self.save(image: thumbnail)
+            }
+        } else {
+            self.save(image: UIImage(named: "robot")!)
+        }
     }
 }
 
@@ -161,6 +166,25 @@ extension ViewController : RedditFeedDelegate {
         if self.collectionView.refreshControl != nil {
             logger(message: "Will stop refreshing.")
             self.collectionView.refreshControl!.endRefreshing()
+        }
+    }
+}
+
+
+extension ViewController {
+    //MARK: - Save image
+
+    func save(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+
+    //MARK: - Save Image callback
+
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            self.logger(message: "An error occured: \(error.localizedDescription )")
+        } else {
+            self.logger(message: "Image was saved successfully.")
         }
     }
 }
